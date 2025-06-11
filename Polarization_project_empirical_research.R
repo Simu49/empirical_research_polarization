9fac0222cf2b9125d6faf18772c259793d652fd7
Simon Bernhard <simonbernhard@gmx.ch>
2025-06-11 10:52:48 +0200

    Update Polarization_project_empirical_research.R

--- Polarization_project_empirical_research.R
+++ Polarization_project_empirical_research.R
@@ -5,11 +5,8 @@
 # University of Bern
 # Spring semester 2024
 
-
 # Preparation -------------------------------------------------------------
-
-
-setwd("C:/Users/Simon Bernhard/OneDrive - Universitaet Bern/Dokumente/Studium/Sozialwissenschaften/HS23 SOWI/Forschungspraktikum Training in Empirical Research/Paper/Data") # set working directory
+# setwd("your working directory") # set working directory
 
 # load necessary libraries
 
@@ -30,18 +27,13 @@ GLES1_to_9 <- read_dta("ZA6838_w1to9_sA_v6-0-0.dta") # read dataset with the wav
 
 GLES19 <- read_dta("ZA6838_w19_sA_v6-0-0.dta") # read dataset of the wave 19
 
-
 # Create dataset -------------------------------------------------
-
-
 dflist <- list(GLES1_to_9, GLES19)
 
 panel <- dflist %>%
   reduce(left_join, by = "lfdn") # merge dataframes
 
-
 # Tidy dataset ------------------------------------------------------------
-
 panel <- panel %>%
   select(
     "lfdn", "kpx_2280.x", "kpx_2290s.x", "kp1_2320", "kp9_2320",
@@ -49,10 +41,8 @@ panel <- panel %>%
     -ends_with("flag")
   ) # only keep relevant variables
 
-
 panel <- panel %>%
   filter(if_all(everything(), ~ . >= 0)) # exclude all missing values (e.g. coded as -97)
-
 
 Panel_long <- panel %>% # format Panel data to long format
   pivot_longer(
@@ -60,7 +50,6 @@ Panel_long <- panel %>% # format Panel data to long format
     names_to = c("wave", ".value"),
     names_sep = "_"
   )
-
 
 variables_with_labels = map(Panel_long, function(x) attr(x, "class") == "haven_labelled") %>% # extract all variable names to a vector to change variabla types from haven_labelled (double labelled) to factor
   names()
@@ -72,14 +61,12 @@ Panel_long_factors <- Panel_long %>% # change Variable types to factors
   mutate_at(all_of(variables_with_labels_to_labels), as_factor) %>%
   mutate_at(all_of(variables_with_labels_to_values), as_factor, "values")
 
-
 Panel_long_factors <- Panel_long_factors %>%
   mutate(wave = as.Date(paste0(ifelse(wave == "kp4", "2017-07",
     ifelse(wave == "kp7", "2017-09",
       ifelse(wave == "kp19", "2021-09", wave)
     )
   ), "-01"))) # format wave as date
-
 
 Panel_long_factors <- Panel_long_factors %>%
   rename(
@@ -88,9 +75,6 @@ Panel_long_factors <- Panel_long_factors %>%
     online_part_like_com = "1616a", online_part_shared = "1616b",
     online_part_post = "1616c", online_part_none = "1616y"
   ) # rename variable to sensible names
-
-
-
 
 Panel_long_factors <- Panel_long_factors %>%
   mutate(
@@ -113,20 +97,14 @@ Panel_long_factors <- Panel_long_factors %>%
     index_active_online_part = online_part_like_com + online_part_shared + online_part_post
   ) # format as numeric, add a polarization variable for left-right, add index for active online participation and convert sex to real dummy
 
-
 # alpha cronbach's reliability score for index
-
 psych::alpha(subset(Panel_long_factors, select = c(online_part_like_com, online_part_shared, online_part_post)), check.keys = TRUE) # alpha cronbach indicates that building an indexvariable is valid
-
-
 
 # save(Panel_long_factors ,file = "Panel_long_factors.Rda")
 
 # load("Panel_long_factors.Rda")
 
-
 # Descriptive analysis ----------------------------------------------------
-
 ggboxplot(Panel_long_factors, x = "wave", y = "soc_med_pol") # boxplots for soc_med_pol distribution across waves
 ggboxplot(Panel_long_factors, x = "wave", y = "polarization") # boxplots for polarization distribution across waves
 
@@ -146,7 +124,6 @@ Panel_long_factors %>%
 # 2017-09-01 2.36 2.63   1
 # 2021-09-01 2.40 2.72   1
 
-
 Panel_long_factors %>%
   group_by(wave) %>%
   summarize(
@@ -161,7 +138,6 @@ Panel_long_factors %>%
 # 2021-09-01 1.53 1.38   1
 
 # stacked Barplot for distribution of polarization across waves
-
 Panel_long_factors %>%
   count(wave, polarization) %>%
   group_by(wave) %>%
@@ -187,7 +163,6 @@ Panel_long_factors %>%
 ggsave("distribution_polarization_stacked.png")
 
 # stacked Barplot for distribution of social media usage across waves
-
 Panel_long_factors %>%
   count(wave, soc_med_pol) %>%
   group_by(wave) %>%
@@ -211,10 +186,8 @@ Panel_long_factors %>%
   labs(fill = "Days per week")
 
 ggsave("distribution_social_media_stacked.png")
-
-
+                            
 # Distribution of social media usage visualized in barplots
-
 Panel_long_factors %>%
   count(wave, soc_med_pol) %>%
   group_by(wave) %>%
@@ -237,13 +210,9 @@ Panel_long_factors %>%
   ylab("Proportion") +
   scale_y_continuous(labels = scales::percent_format())
 
-
 ggsave("distribution_social_media.png")
 
-
 # Distribution of polarization variable visualized in barplots
-
-
 Panel_long_factors %>%
   count(wave, left_right_self) %>%
   group_by(wave) %>%
@@ -269,9 +238,7 @@ Panel_long_factors %>%
 
 ggsave("distribution_left_right.png")
 
-
 # Distribution of polarization variable visualized in barplots
-
 Panel_long_factors %>%
   count(wave, polarization) %>%
   group_by(wave) %>%
@@ -295,9 +262,7 @@ Panel_long_factors %>%
 
 ggsave("distribution_polarization.png")
 
-
 # Boxplots of the distribution of political polarization by social media usage across the whole panel
-
 ggplot(Panel_long_factors, aes(x = factor(soc_med_pol), y = polarization)) +
   geom_boxplot() +
   labs(
@@ -316,10 +281,7 @@ ggplot(Panel_long_factors, aes(x = factor(soc_med_pol), y = polarization)) +
 
 ggsave("distribution_polarization_social_media_usage.png")
 
-
-
 # Panel Regression without controls ----------------------------------------------
-
 Panel_polarization <- pdata.frame(Panel_long_factors, index = c("lfdn", "wave")) # transform dataframe to panel dataframe
 
 POLS_nc <- plm(polarization ~ soc_med_pol + index_active_online_part, model = "pooling", data = Panel_polarization) # Pooled OLS
@@ -334,7 +296,6 @@ phtest(fixed_effects_nc, random_effects_nc) # Hausman test indicates fixed effec
 summary(fixed_effects_nc)
 
 # Panel Regression with controls ------------------------------------------
-
 POLS_wc <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, model = "pooling", data = Panel_polarization) # Pooled OLS
 
 bptest(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, data = Panel_polarization, studentize = F) # Breusch pagan test p-value < 2.2e-16 --> heteroscedasticity is present
@@ -345,20 +306,15 @@ phtest(fixed_effects_wc, random_effects_wc) # Hausman test indicates fixed effec
 
 summary(fixed_effects_wc)
 
-
 # Compute cluster-robust standard errors
-
 cluster_robust_se_nc <- vcovHC(fixed_effects_nc, type = "HC1", cluster = "group")
 cluster_robust_se_wc <- vcovHC(fixed_effects_wc, type = "HC1", cluster = "group")
 
 # Extract standard errors
-
 se_nc <- sqrt(diag(cluster_robust_se_nc))
 se_wc <- sqrt(diag(cluster_robust_se_wc))
 
-
 # Export Regression table with stargazer
-
 stargazer(fixed_effects_nc, fixed_effects_wc,
   column.labels = c("without controls", "with controls"),
   se = list(se_nc, se_wc),
@@ -369,9 +325,7 @@ stargazer(fixed_effects_nc, fixed_effects_wc,
   out = "models_fixed_effects.html"
 )
 
-
 # Panel Regression grouped -----------------------------------------------
-
 Filter_panel <- Panel_long_factors[!duplicated(Panel_long_factors$lfdn), ] # get first observation of each case in a dataframe to filter cases
 
 filter_panel_left <- Filter_panel %>%
@@ -385,12 +339,9 @@ filter_panel_middle <- Filter_panel %>%
 
 Panel_left <- semi_join(Panel_long_factors, filter_panel_left, by = "lfdn")
 
-
 Panel_right <- semi_join(Panel_long_factors, filter_panel_right, by = "lfdn")
 
-
 Panel_middle <- semi_join(Panel_long_factors, filter_panel_middle, by = "lfdn")
-
 
 Panel_polarization_left <- pdata.frame(Panel_left, index = c("lfdn", "wave")) # transform dataframe to panel dataframe
 
@@ -398,15 +349,12 @@ Panel_polarization_right <- pdata.frame(Panel_right, index = c("lfdn", "wave")) 
 
 Panel_polarization_middle <- pdata.frame(Panel_middle, index = c("lfdn", "wave")) # transform dataframe to panel dataframe
 
-
 # left group
-
 POLS_left <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2,
   model = "pooling", data = Panel_polarization_left
 )
 
 bptest(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, data = Panel_polarization_left, studentize = F) # Breusch pagan test p-value = 3.01e-11 --> heteroscedasticity is present
-
 
 fixed_effects_left <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, data = Panel_polarization_left, index = c("lfdn", "wave"), model = "within") # fixed model
 random_effects_left <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, data = Panel_polarization_left, index = c("lfdn", "wave"), model = "random") # random model
@@ -414,16 +362,12 @@ phtest(fixed_effects_left, random_effects_left) # Hausman test indicates fixed e
 
 summary(fixed_effects_left)
 
-
 # right group
-
 POLS_right <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2,
   model = "pooling", data = Panel_polarization_right
 )
 
 bptest(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, data = Panel_polarization_right, studentize = F) # Breusch pagan test p-value = 9.194e-16 --> heteroscedasticity is present
-
-
 
 fixed_effects_right <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, data = Panel_polarization_right, index = c("lfdn", "wave"), model = "within") # fixed model
 random_effects_right <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2, data = Panel_polarization_right, index = c("lfdn", "wave"), model = "random") # random model
@@ -431,9 +375,7 @@ phtest(fixed_effects_right, random_effects_right) # Hausman test indicates fixed
 
 summary(fixed_effects_right)
 
-
 # middle group
-
 POLS_middle <- plm(polarization ~ soc_med_pol + index_active_online_part + pol_interest + sex + agecategory + edu2,
   model = "pooling", data = Panel_polarization_middle
 )
@@ -446,22 +388,17 @@ phtest(fixed_effects_middle, random_effects_middle) # Hausman test indicates fix
 
 summary(fixed_effects_middle)
 
-
 # Compute cluster-robust standard errors for each model
-
 cluster_robust_se_left <- vcovHC(fixed_effects_left, type = "HC1", cluster = "group")
 cluster_robust_se_middle <- vcovHC(fixed_effects_middle, type = "HC1", cluster = "group")
 cluster_robust_se_right <- vcovHC(fixed_effects_right, type = "HC1", cluster = "group")
 
 # Extract standard errors
-
 se_left <- sqrt(diag(cluster_robust_se_left))
 se_middle <- sqrt(diag(cluster_robust_se_middle))
 se_right <- sqrt(diag(cluster_robust_se_right))
 
-
 # Export Regression table with stargazer
-
 stargazer(fixed_effects_left, fixed_effects_middle, fixed_effects_right,
   column.labels = c("left subsample", "middle subsample", "right subsample"),
   se = list(se_left, se_middle, se_right),
